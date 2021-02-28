1866930bc90d011db0b155c7af58feb8b547fdcd
Md. Ashiqur Rahman <62802119+ashiq120@users.noreply.github.com>
2021-02-28 21:06:16 +0600

    Add files via upload
    First Commit

---
+++ login.php
@@ -0,0 +1,60 @@
+<?php
+   // $filepath("Information.txt")
+    
+    $file=fopen("Information.txt","r");
+    while(!feof($file)) {
+  echo fgets($file) . "<br>";
+}
+    
+?>
+<html>
+
+<head>
+    <title> Login</title>
+</head>
+<body>
+<form method ="POST">
+
+<table>
+     <tr>
+         <td>
+             UserName
+         </td>
+        <td>
+             <input type="text" name="uname">
+          </td>
+    </tr>
+    <tr>
+         <td>
+             Password
+        </td>
+        <td>
+             <input type="Password" name="pwd">
+         </td>
+    </tr>
+    <tr>
+         <td>
+             <input  type="submit" name="Login" value="Login">
+        </td>
+    </tr>
+                
+
+      <tr>
+       <td>
+             <input  type="submit" name="SignUp" value="SignUp">
+         </td>        
+     </tr>
+ <?php
+
+if(isset($_POST["SignUp"]))
+{
+    echo "<script>location.href='FileHandling.php'</script>";
+}
+if(isset($_POST["Login"]))
+{
+ echo "<script>location.href='welcomepage.php'</script>";
+}
+?>
+</form>
+</body>
+</html>
